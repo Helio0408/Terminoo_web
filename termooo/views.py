7a6eabb4fc6d0293178cb059da47c61a9d92a418
Helio <heliosantos0408@gmail.com>
2024-12-16 18:44:55 -0300

    limite de tentativas

--- termooo/views.py
+++ termooo/views.py
@@ -14,14 +14,29 @@ def start_game(request):
 def play_game(request, game_id):
     game = get_object_or_404(Game, id=game_id)
     feedback = None
+    message = None  # Inicializa a variável message
 
-    if request.method == "POST" and not game.is_completed:
-        attempt = request.POST["attempt"]
-        feedback = evaluate_attempt(attempt, game.target_word.text)
-        game.attempts.append({"word": attempt, "feedback": feedback})
-        if attempt == game.target_word.text:
-            game.is_completed = True
-        game.save()
+    # Verifica se o jogo foi completado ou se as tentativas acabaram
+    if game.is_completed:
+        if game.attempts[-1]["word"] == game.target_word.text:
+            message = "Parabéns! Você acertou!"
+        else:
+            message = "Você perdeu! Tentativas esgotadas."
+    else:
+        # Se o jogo ainda não terminou e o número de tentativas é menor que 6
+        if len(game.attempts) < 6:
+            if request.method == "POST":
+                attempt = request.POST["attempt"]
+                feedback = evaluate_attempt(attempt, game.target_word.text)
+                game.attempts.append({"word": attempt, "feedback": feedback})
+                if attempt == game.target_word.text:
+                    game.is_completed = True
+                game.save()
+        # Caso o número máximo de tentativas tenha sido alcançado
+        if len(game.attempts) >= 6 and not game.is_completed:
+            message = "Você perdeu! Tentativas esgotadas."
+            game.is_completed = True  # Marca o jogo como terminado
+            game.save()
 
-    return render(request, "game/play_game.html", {"game": game, "feedback": feedback})
+    return render(request, "game/play_game.html", {"game": game, "feedback": feedback, "message": message})
 
