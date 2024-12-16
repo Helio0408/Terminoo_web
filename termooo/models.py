6d2bf424b10dbc029f70779904fb0b4d4e660ea6
gabrielmtins <gabrielmartinsthe12381@gmail.com>
2024-12-16 17:08:05 -0300

    adicionado database

--- termooo/models.py
+++ termooo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Palavra(models.Model):
+    texto = models.CharField(max_length=100, unique=True, verbose_name="Palavra")
+
+    class Meta:
+        verbose_name = "Palavra"
+        verbose_name_plural = "Palavras"
+        ordering = ['texto']
+
+    def __str__(self):
+        return f"{self.texto}"
